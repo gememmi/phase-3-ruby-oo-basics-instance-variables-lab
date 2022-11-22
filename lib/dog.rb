05491c06ab52c3879d2e2ba5b709a2711129e4f4
Emily <emelizadaniels@gmail.com>
2022-11-22 13:28:16 -0500

    commit tree 870ad1e7a6d275159c679e8cde0a3932c63e728b

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,3 +1,20 @@
+require 'pry'
 class Dog
+    # SETTER
+    def name=(value)
+       @name = value
+      end
 
+    # GETTER
+      def name
+        @name
+      end
 end
+lassie = Dog.new
+lassie.name = "Lassie"
+puts lassie.name
+
+#
+
+
+
